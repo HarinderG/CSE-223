4f479583b43e2f192113017b88837d55fea41ab3
harinderg <harinder.gakhal@gmail.com>
2019-04-15 17:22:52 -0700

    Use final test cases in Main

--- Programming Assignments/pa1/Main.java
+++ Programming Assignments/pa1/Main.java
@@ -7,36 +7,50 @@
 */
 
 class Main{
-	public static void main(String[] args)
-	{
-		Fraction a=new Fraction(1,2); // a=1/2 
-		Fraction b=new Fraction(4); // b=4
-		Fraction c,d,e;
-		System.out.println("5/2: "+new Fraction(125,50));
-		c=a.add(b);
-		System.out.println("1: " + new Fraction(1,2).mul(new Fraction(2))); System.out.println("9/2: "+c);
-		c=a.sub(b);
-		System.out.println("-7/2: "+c);
-		c=b.sub(a);
-		System.out.println("7/2: "+c);
-		c=a.mul(b);
-		System.out.println("2: "+c);
-		System.out.println("4: "+c.mul(c));
-		d=new Fraction(3); // d=3
-		e=new Fraction(4); // e=4
-		c=d.div(e);
-		System.out.println("3/4: "+c);
-		a=e.div(d);
-		System.out.println("4/3: "+a);
-		c=a.mul(b); // c=16/3
-		System.out.println("16/3: "+c); System.out.println("1/3=0.33333...:"+ new Fraction(1,3).toDouble()); System.out.println("1/0 (NaN): "+new Fraction(1,0));
-		a=new Fraction(1,2); // a=1/2
-		b=new Fraction(1,2); // b=1/2
-		c=a.sub(b); // c=0
-		a=a.div(c); // a=1/0
-		System.out.println("NaN: "+a.toDouble());
-		a=c.div(c); // a=0/0
-		System.out.println("NaN: "+a.toDouble());
-		System.out.println("NaN: "+new Fraction(-1,0).toDouble());
+	  public static void main(String[] args)
+	  {
+	    Fraction a=new Fraction(1,2);
+	    Fraction b=new Fraction(2);
+	    Fraction c=new Fraction(2,3);
+	    System.out.println("1/2: " + a);
+	    System.out.println("2: " + b);
+	    System.out.println("2/3: " + c);
+	    System.out.println("Mul: 1/3 "+a.mul(c));
+	    System.out.println("Mul: 1/3 "+c.mul(a));
+	    System.out.println("Reduce: 1 "+b.mul(a));
+	    System.out.println("Reduce: 1 "+a.mul(b));
+	    System.out.println("Div: 3/4 "+a.div(c));
+	    System.out.println("Div: 4/3 "+c.div(a));
+	    System.out.println("Add: 7/6 "+a.add(c));
+	    System.out.println("Add: 7/6 "+c.add(a));
+	    System.out.println("Sub: -1/6 "+a.sub(c));
+	    System.out.println("Sub: 1/6 "+c.sub(a));
+	    System.out.println("x-x=0: "+c.sub(c));
+
+	    Fraction z=new Fraction(0,1);
+	    System.out.println("0: "+z);
+	    System.out.println("0: "+z.mul(a));
+	    System.out.println("0: "+a.mul(z));
+
+	    System.out.println("toDouble:.6667 "+c.toDouble());
+
+	    System.out.println("-2/3 " + new Fraction(2,-3));
+	    System.out.println("-2/3 " + new Fraction(-2,3));
+	    System.out.println("2/3 " + new Fraction(-2,-3));
+
+	    Fraction q=new Fraction(-3,-12);
+	    q=q.add(q.add(q.add(q)));
+	    System.out.println("1=" + q);
+	    q=new Fraction(-3,-12);
+	    q=q.add(q.add(q.add(q.add(q))));
+	    System.out.println("5/4=" + q);
+
+	    Fraction n1=new Fraction(1,0);
+	    Fraction n2=new Fraction(0,0);
+	    Fraction n3=new Fraction(-20,0);
+	    Fraction n4=new Fraction(24,0);
+	    System.out.println("4 NaNs: " + n1 + " " + n2 + " " + n3 + " " + n4);
+	    System.out.println("4 NaN toDouble: " + n1.toDouble() + " " + n2.toDouble() + " " + n3.toDouble() + " " + n4.toDouble());
+	    System.out.println("4 NaNs: " + a.add(n1) + " " + a.mul(n2) + " " + n3.sub(n3) + " " + new Fraction(0,1).mul(n4));
+	  }
 	}
-}
